8622d0508068bf6a770639a567d0b1bcf3802788
SuryaTeja Duggi <suryateja008@gmail.com>
2016-03-05 15:52:43 -0800

    Model files with profe student and user class created

--- src/quizaApp/Model/Login.java
+++ src/quizaApp/Model/Login.java
@@ -3,5 +3,23 @@ package quizaApp.Model;
 /**
  * Created by suryaduggi on 3/5/16.
  */
-public class Login {
+public class Login extends DBconnect {
+
+    private String userName = "Surya";
+    private String passCode = "Teja";
+    private int id = 1234;
+    private int privilage = 0;
+    private int preference = 0;
+
+    public User validateData(String userName, String passCode)
+    {
+        User user;
+        if(userName==this.userName && passCode==this.passCode && preference == 0){
+            user = new Student(id,userName, preference);
+        }
+        else {
+            user = new Professor(id,userName,preference);
+        }
+        return user;
+    }
 }
---
+++ src/quizaApp/Model/Professor.java
@@ -0,0 +1,14 @@
+package quizaApp.Model;
+
+/**
+ * Created by suryaduggi on 3/5/16.
+ */
+public class Professor extends User {
+
+    Professor(int id,String name,int privilage)
+    {
+        this.setId(id);
+        this.setName(name);
+        this.setPrivilage(privilage);
+    }
+}
