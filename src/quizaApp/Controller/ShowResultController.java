99064fc98553fa65a0c630a2f9d44625411f0f24
SuryaTeja Duggi <suryateja008@gmail.com>
2016-03-07 01:52:03 -0800

    resultView

--- src/quizaApp/Controller/ShowResultController.java
+++ src/quizaApp/Controller/ShowResultController.java
@@ -2,14 +2,18 @@ package quizaApp.Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuButton;
+import quizaApp.Model.DBconnect;
 import quizaApp.Model.Professor;
 
 public class ShowResultController {
+
+	private final DBconnect db;
+    private final Professor professor;
 	public ShowResultController(Professor professor) {
 		// TODO Auto-generated constructor stub
+		this.professor = professor;
+		db = new DBconnect();
 	}
 
-	@FXML
-	public MenuButton viewResult;
 
 }
