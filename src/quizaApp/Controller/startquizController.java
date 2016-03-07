caf8d09ddf1adee97ae8be272cb800bed540bbf1
vinodsamanth <vinodnrssamanth@gmail.com>
2016-03-06 18:46:07 -0800

    commit

--- src/quizaApp/Controller/startquizController.java
+++ src/quizaApp/Controller/startquizController.java
@@ -3,6 +3,8 @@ package quizaApp.Controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -19,7 +21,11 @@ public class startquizController implements Initializable {
 	final Quiz quiz;
 	final Question[] questions;
 	private Option[] options;
-
+	BooleanProperty isLast;
+	int[] selected;
+	int numCorrect;
+	
+	
 	@FXML
 	private Label questionName;
 	@FXML
@@ -40,12 +46,9 @@ public class startquizController implements Initializable {
 	private RadioButton rThree;
 	@FXML
 	private RadioButton rFour;
-	@FXML
-	private Button previous;
+	
 	@FXML
 	private Button next;
-	@FXML
-	private Button end;
 	@FXML
 	private Button finalSubmit;
 	
@@ -56,24 +59,63 @@ public class startquizController implements Initializable {
 		this.student = student;
 		this.quiz = quiz;
 		this.questions = quiz.getQuestions();
+		this.selected = new int[quiz.getNoOfQuestions()];
+		this.isLast = new SimpleBooleanProperty(true);
+		this.numCorrect = 0;
 	}
 	
+	@FXML
 	public void nextQuestion(){
-		options = questions[0].getOptions();
-		questionName.setText(questions[0].getQuestion());
+		for(int i = 0; i < 5; i ++){
+			if(rOne.isSelected()){
+				selected[count] = 1;
+				rOne.setSelected(false);
+			}
+			else if(rTwo.isSelected()){
+				selected[count] = 2;
+				rTwo.setSelected(false);
+			}
+			else if(rThree.isSelected()){
+				selected[count] = 3;
+				rThree.setSelected(false);
+			}
+			else{
+				selected[count] = 4;
+				rFour.setSelected(false);
+			}
+		}
+		System.out.println(selected[count]);
+		count++;
+		System.out.println(count+ " : " + quiz.getNoOfQuestions());
+		if(count == quiz.getNoOfQuestions()-1){
+			isLast.set(false);
+		}
+		options = questions[count].getOptions();
+		questionName.setText(questions[count].getQuestion());
 		optionOne.setText(options[0].getOptionString());
 		optionTwo.setText(options[1].getOptionString());
 		optionThree.setText(options[2].getOptionString());
 		optionFour.setText(options[3].getOptionString());
 	}
 	
+	@FXML
+	public void submitQuiz(){
+		for(int i = 0; i < quiz.getNoOfQuestions(); i++){
+			options = questions[i].getOptions();
+			if(options[selected[i]].isAnswer()){
+				numCorrect++;
+			}
+		}
+	}
+	
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		next.visibleProperty().bind(isLast);
 		count = 0;
-		options = questions[0].getOptions();
-		questionName.setText(questions[0].getQuestion());
+		options = questions[count].getOptions();
+		questionName.setText(questions[count].getQuestion());
 		optionOne.setText(options[0].getOptionString());
 		optionTwo.setText(options[1].getOptionString());
 		optionThree.setText(options[2].getOptionString());
