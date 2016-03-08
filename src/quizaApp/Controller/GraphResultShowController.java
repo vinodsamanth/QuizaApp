4d49664221b6d3ba42eaaa4ef4fefbb0dbe2c121
vinodsamanth <vinodnrssamanth@gmail.com>
2016-03-07 16:15:07 -0800

    goHome button added on Graph view

--- src/quizaApp/Controller/GraphResultShowController.java
+++ src/quizaApp/Controller/GraphResultShowController.java
@@ -1,5 +1,6 @@
 package quizaApp.Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -8,9 +9,14 @@ import javax.xml.crypto.Data;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.StackPane;
+import javafx.util.Callback;
+import quizaApp.Main;
 import quizaApp.Model.DBconnect;
 import quizaApp.Model.Professor;
 
@@ -31,6 +37,30 @@ public class GraphResultShowController implements Initializable{
 	}
 
 
+	@FXML
+	public void goHome(){
+		final ProfController ProfController = new ProfController(professor);
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("/quizaApp/view/Prof.fxml"));
+		loader.setControllerFactory(new Callback<Class<?>, Object>() {
+			
+			@Override
+			public Object call(Class<?> arg0) {
+				// TODO Auto-generated method stub
+				return ProfController;
+			}
+		});
+		TitledPane rootLayout= null;
+		try {
+			rootLayout = (TitledPane) loader.load();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Scene scene = new Scene(rootLayout);
+		Main.mainStage.setScene(scene);
+	}
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
